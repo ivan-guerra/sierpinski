f992b76a30acface7539d89a0c95f549a2a7abea
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-02-04 11:20:30 -0800

    Add functions for controlling the screen and drawing triangles.

---
+++ include/graphics/screen.h
@@ -0,0 +1,56 @@
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+#include <optional>
+#include <vector>
+
+#include "common/types.h"
+
+namespace sierpinski {
+namespace graphics {
+
+/**
+ * \brief Screen Dimensions
+ */
+struct ScreenDimension {
+  int width = 0;  /**< Screen width */
+  int height = 0; /**< Screen height */
+};
+
+/**
+ * \brief Initialize the ncurses screen.
+ * \returns The dimensions of the screen (i.e., terminal window).
+ */
+[[nodiscard]] std::optional<ScreenDimension> InitScreen() noexcept;
+
+/**
+ * \brief Cleanup ncurses window(s).
+ */
+void TerminateScreen() noexcept;
+
+/**
+ * \brief Clear the screen.
+ */
+void Clear() noexcept;
+
+/**
+ * \brief Render all triangles in \p triangles on screen.
+ * \param [in] triangles A set of Sierpinski triangles to be rendered on screen.
+ */
+void DrawObject(const std::vector<Triangle>& triangles) noexcept;
+
+/**
+ * \brief Print a help message on screen.
+ * \param [in] screen_dim Screen (i.e., terminal) dimensions.
+ */
+void DrawInstructions(const ScreenDimension& screen_dim) noexcept;
+
+/**
+ * \brief Return \c true if the user pressed 'q' to quit the app.
+ */
+[[nodiscard]] std::optional<bool> UserRequestedToQuit() noexcept;
+
+}  // namespace graphics
+}  // namespace sierpinski
+
+#endif
---
+++ src/graphics/screen.cpp
@@ -0,0 +1,60 @@
+#include "graphics/screen.h"
+
+#include <curses.h>
+#include <ncurses.h>
+
+#include <optional>
+
+namespace sierpinski {
+namespace graphics {
+
+std::optional<ScreenDimension> InitScreen() noexcept {
+  if (!::initscr()) {
+    return std::nullopt;
+  }
+  /*
+   * In the conditional below, we try to set the following ncurses settings:
+   *   (1) Disable line buffering.
+   *   (2) Enable the keypad so we can exit via keypress.
+   *   (3) Disable character echoing.
+   *   (4) Hide the cursor.
+   */
+  if (ERR == ::cbreak() || ERR == ::keypad(stdscr, TRUE) || ERR == ::noecho() ||
+      ERR == ::curs_set(0)) {
+    return std::nullopt;
+  }
+
+  /* Fetch the screen dimensions. */
+  ScreenDimension screen_dim = {.width = 0, .height = 0};
+
+  /* Funny, getmaxyx() macro has an undefined return value. */
+  getmaxyx(stdscr, screen_dim.height, screen_dim.width);
+
+  return screen_dim;
+}
+
+void TerminateScreen() noexcept { ::endwin(); }
+
+void Clear() noexcept { ::clear(); }
+
+void DrawObject(const std::vector<Triangle>& triangles) noexcept {
+  for (const Triangle& triangle : triangles) {
+    for (const Point2D& vertex : triangle.vertices) {
+      mvaddch(vertex.y, vertex.x, '*');
+    }
+  }
+  ::refresh();
+}
+
+void DrawInstructions(const ScreenDimension& screen_dim) noexcept {
+  ::mvprintw(screen_dim.height - 1, 0, "%s", "press q to quit");
+  ::refresh();
+}
+
+std::optional<bool> UserRequestedToQuit() noexcept {
+  int c = ::getch();
+  return (ERR == c) ? std::nullopt : std::optional<bool>(c == 'q');
+}
+
+}  // namespace graphics
+}  // namespace sierpinski
