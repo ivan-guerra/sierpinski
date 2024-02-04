951dab5836c00dbaa61f5a77cfa422ec573bb798
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-02-04 11:47:06 -0800

    Add additional args for triangle side length and screen refresh rate.

--- src/sierpinski/sierpinski.cpp
+++ src/sierpinski/sierpinski.cpp
@@ -7,14 +7,29 @@
 
 static void PrintUsage() noexcept {
   std::cout << "usage: sierpinski [OPTION]..." << std::endl;
-  std::cout << "an ncurses of rendering sierpinski's triangle" << std::endl;
-  std::cout << "\t-d, --degree\tfractal degree" << std::endl;
-  std::cout << "\t-h, --help\tprint this help page" << std::endl;
+  std::cout << "an ncurses rendering of sierpinski's triangle" << std::endl;
+  std::cout << "\t-d, --degree\t\tfractal degree" << std::endl;
+  std::cout << "\t-s, --side-length\tinitial triangle side length" << std::endl;
+  std::cout << "\t-r, --refresh-rate\tscreen refresh rate in seconds"
+            << std::endl;
+  std::cout << "\t-h, --help\t\tprint this help page" << std::endl;
 }
 
 static void PrintErrAndExit(const std::string& err_msg) noexcept {
   std::cerr << "error: " << err_msg << std::endl;
   std::exit(EXIT_FAILURE);
+}
+
+[[nodiscard]] static unsigned int ParseUnsignedInt(const char* optarg) {
+  unsigned int value = 0;
+  try {
+    value = std::stoul(optarg);
+  } catch (const std::invalid_argument& e) {
+    PrintErrAndExit("cannot convert '" + std::string(optarg) + "' to uint");
+  } catch (const std::out_of_range& e) {
+    PrintErrAndExit(std::string(optarg) + " is out of uint range");
+  }
+  return value;
 }
 
 int main(int argc, char** argv) {
@@ -27,19 +42,20 @@ int main(int argc, char** argv) {
   int opt = 0;
   int long_index = 0;
   unsigned int degree = 0;
-  while (-1 != (opt = ::getopt_long(argc, argv, "hd:",
+  unsigned int side_length = 10;
+  unsigned int refresh_rate_sec = 1;
+  while (-1 != (opt = ::getopt_long(argc, argv, "hd:s:r:",
                                     static_cast<struct option*>(long_options),
                                     &long_index))) {
     switch (opt) {
       case 'd':
-        try {
-          degree = std::stoul(optarg);
-        } catch (const std::invalid_argument& e) {
-          PrintErrAndExit("cannot convert '" + std::string(optarg) +
-                          "' to uint");
-        } catch (const std::out_of_range& e) {
-          PrintErrAndExit("degree value is out of uint range");
-        }
+        degree = ParseUnsignedInt(optarg);
+        break;
+      case 's':
+        side_length = ParseUnsignedInt(optarg);
+        break;
+      case 'r':
+        refresh_rate_sec = ParseUnsignedInt(optarg);
         break;
       case 'h':
         PrintUsage();
