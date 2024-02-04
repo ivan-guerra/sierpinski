00dd0f01e99dd9fa82071490a35099b6157ac38d
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-02-04 12:45:49 -0800

    Change the screen refresh rate to be in units of milliseconds.

--- src/sierpinski/sierpinski.cpp
+++ src/sierpinski/sierpinski.cpp
@@ -12,7 +12,7 @@ static void PrintUsage() noexcept {
   std::cout << "an ncurses rendering of sierpinski's triangle" << std::endl;
   std::cout << "\t-d, --degree\t\tfractal degree" << std::endl;
   std::cout << "\t-s, --side-length\tinitial triangle side length" << std::endl;
-  std::cout << "\t-r, --refresh-rate\tscreen refresh rate in seconds"
+  std::cout << "\t-r, --refresh-rate\tscreen refresh rate in milliseconds"
             << std::endl;
   std::cout << "\t-h, --help\t\tprint this help page" << std::endl;
 }
@@ -35,7 +35,7 @@ static void PrintErrAndExit(const std::string& err_msg) noexcept {
 }
 
 static void RunDrawLoop(unsigned int degree, unsigned int side_length,
-                        unsigned int refresh_rate_sec) noexcept {
+                        unsigned int refresh_rate_ms) noexcept {
   /* Setup a blank screen. */
   std::optional<sierpinski::graphics::ScreenDimension> screen_dim =
       sierpinski::graphics::InitScreen();
@@ -45,7 +45,7 @@ static void RunDrawLoop(unsigned int degree, unsigned int side_length,
 
   /* The refresh rate drives how long we wait for user input before increasing
    * the degree of the fractal and drawing it on screen. */
-  sierpinski::graphics::EnableInputDelay(refresh_rate_sec * 1000);
+  sierpinski::graphics::EnableInputDelay(refresh_rate_ms);
 
   (void)degree;
   (void)side_length;
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
   int long_index = 0;
   unsigned int degree = 0;
   unsigned int side_length = 10;
-  unsigned int refresh_rate_sec = 1;
+  unsigned int refresh_rate_ms = 100;
   while (-1 != (opt = ::getopt_long(argc, argv, "hd:s:r:",
                                     static_cast<struct option*>(long_options),
                                     &long_index))) {
@@ -81,7 +81,7 @@ int main(int argc, char** argv) {
         side_length = ParseUnsignedInt(optarg);
         break;
       case 'r':
-        refresh_rate_sec = ParseUnsignedInt(optarg);
+        refresh_rate_ms = ParseUnsignedInt(optarg);
         break;
       case 'h':
         PrintUsage();
@@ -92,7 +92,7 @@ int main(int argc, char** argv) {
     }
   }
 
-  RunDrawLoop(degree, side_length, refresh_rate_sec);
+  RunDrawLoop(degree, side_length, refresh_rate_ms);
 
   std::exit(EXIT_SUCCESS);
 }
