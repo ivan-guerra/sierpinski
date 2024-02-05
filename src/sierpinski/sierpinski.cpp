3004d2a4c8efe0fcd3dcb750fc6c855e765fd153
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-02-04 19:39:11 -0800

    Add the core Sierpinski triangle drawing logic.

--- src/sierpinski/sierpinski.cpp
+++ src/sierpinski/sierpinski.cpp
@@ -12,10 +12,8 @@
 static void PrintUsage() noexcept {
   std::cout << "usage: sierpinski [OPTION]..." << std::endl;
   std::cout << "an ncurses rendering of sierpinski's triangle" << std::endl;
-  std::cout << "\t-d, --degree\t\tfractal degree" << std::endl;
-  std::cout << "\t-r, --refresh-rate\tscreen refresh rate in milliseconds"
-            << std::endl;
-  std::cout << "\t-h, --help\t\tprint this help page" << std::endl;
+  std::cout << "\t-d, --degree\tfractal degree (default 2)" << std::endl;
+  std::cout << "\t-h, --help\tprint this help page" << std::endl;
 }
 
 static void PrintErrAndExit(const std::string& err_msg) noexcept {
@@ -35,8 +33,36 @@ static void PrintErrAndExit(const std::string& err_msg) noexcept {
   return value;
 }
 
-static void RunDrawLoop(unsigned int degree,
-                        unsigned int refresh_rate_ms) noexcept {
+static void Sierpinski(const sierpinski::Triangle& triangle,
+                       int degree) noexcept {
+  sierpinski::graphics::DrawTriangle(triangle);
+  if (degree) {
+    const sierpinski::Point2D& v0 = triangle.vertices[0];
+    const sierpinski::Point2D& v1 = triangle.vertices[1];
+    const sierpinski::Point2D& v2 = triangle.vertices[2];
+
+    sierpinski::Triangle s1;
+    s1.vertices[0] = v0;
+    s1.vertices[1] = sierpinski::util::MidPoint(v0, v1);
+    s1.vertices[2] = sierpinski::util::MidPoint(v0, v2);
+
+    sierpinski::Triangle s2;
+    s2.vertices[0] = v1;
+    s2.vertices[1] = sierpinski::util::MidPoint(v0, v1);
+    s2.vertices[2] = sierpinski::util::MidPoint(v1, v2);
+
+    sierpinski::Triangle s3;
+    s3.vertices[0] = v2;
+    s3.vertices[1] = sierpinski::util::MidPoint(v2, v1);
+    s3.vertices[2] = sierpinski::util::MidPoint(v0, v2);
+
+    Sierpinski(s1, degree - 1);
+    Sierpinski(s2, degree - 1);
+    Sierpinski(s3, degree - 1);
+  }
+}
+
+static void DrawSierpinskiTriangles(unsigned int degree) noexcept {
   /* Setup a blank screen. */
   std::optional<sierpinski::graphics::ScreenDimension> screen_dim =
       sierpinski::graphics::InitScreen();
@@ -44,19 +70,27 @@ static void RunDrawLoop(unsigned int degree,
     PrintErrAndExit("failed to initialize screen");
   }
 
-  /* The refresh rate drives how long we wait for user input before increasing
-   * the degree of the fractal and drawing it on screen. */
-  sierpinski::graphics::EnableInputDelay(refresh_rate_ms);
+  /* Construction of the initial triangle shown on the screen. This is
+   * the fractal with degree = 0. The code below tries to make the initial
+   * triangle fill the entire screen. */
+  sierpinski::Point2D screen_center = {.x = screen_dim->width / 2,
+                                       .y = screen_dim->height / 2};
+  int max_dist =
+      std::min(screen_dim->width / 2 - 1, screen_dim->height / 2 - 1);
+  sierpinski::Triangle triangle =
+      sierpinski::util::CreateCenteredTriangle(screen_center, max_dist);
 
-  sierpinski::Triangle triangle = sierpinski::util::CreateCenteredTriangle(
-      {.x = screen_dim->width / 2, .y = screen_dim->height / 2}, degree);
-  while (!sierpinski::graphics::UserRequestedToQuit()) {
-    sierpinski::graphics::DrawTriangle(triangle);
-    sierpinski::graphics::DrawInstructions(*screen_dim);
+  /* The actual algorithm magic happens here. */
+  Sierpinski(triangle, degree);
+
+  /* Place prompt telling the user how to exit. */
+  sierpinski::graphics::DrawInstructions(*screen_dim);
+
+  /* Wait for the user to press a key before cleaning up. */
+  while (!sierpinski::graphics::UserPressedKey()) {
   }
 
   /* Cleanup. */
-  sierpinski::graphics::DisableInputDelay();
   sierpinski::graphics::TerminateScreen();
 }
 
@@ -69,17 +103,13 @@ int main(int argc, char** argv) {
 
   int opt = 0;
   int long_index = 0;
-  unsigned int degree = 8;
-  unsigned int refresh_rate_ms = 100;
+  unsigned int degree = 2;
   while (-1 != (opt = ::getopt_long(argc, argv, "hd:s:r:",
                                     static_cast<struct option*>(long_options),
                                     &long_index))) {
     switch (opt) {
       case 'd':
         degree = ParseUnsignedInt(optarg);
-        break;
-      case 'r':
-        refresh_rate_ms = ParseUnsignedInt(optarg);
         break;
       case 'h':
         PrintUsage();
@@ -90,7 +120,7 @@ int main(int argc, char** argv) {
     }
   }
 
-  RunDrawLoop(degree, refresh_rate_ms);
+  DrawSierpinskiTriangles(degree);
 
   std::exit(EXIT_SUCCESS);
 }
