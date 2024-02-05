d443f394a8a75156cfd272169626ca3ce1c640a5
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-02-04 23:32:58 -0800

    Remove unused resource directory definition.

--- scripts/config.sh
+++ scripts/config.sh
@@ -9,8 +9,5 @@ export SIERPINSKI_SCRIPTS_PATH="${SIERPINSKI_PROJECT_PATH}/scripts/linux"
 # Binary directory.
 export SIERPINSKI_BIN_DIR="${SIERPINSKI_PROJECT_PATH}/bin"
 
-# Resource directory.
-export SIERPINSKI_RES_DIR="${SIERPINSKI_PROJECT_PATH}/resources"
-
 # CMake build files and cache.
 export SIERPINSKI_BUILD_DIR="${SIERPINSKI_PROJECT_PATH}/build"
